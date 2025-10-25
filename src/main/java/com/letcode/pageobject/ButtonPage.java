02f373640c90d0260b8a85cc004e1438ac751ab8
KarthikBalasubramaian <karthiktn3637@gmail.com>
2025-10-25 16:46:32 +0530

    Feat(Button Page Tests): add automation test scripts for button page tests

--- src/main/java/com/letcode/pageobject/ButtonPage.java
+++ src/main/java/com/letcode/pageobject/ButtonPage.java
@@ -2,6 +2,7 @@ package com.letcode.pageobject;
 
 
 
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.Point;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -10,32 +11,54 @@ import org.openqa.selenium.support.PageFactory;
 import com.letcode.baseclass.Basesteps;
 
 public class ButtonPage extends Basesteps {
-	
+
 	@FindBy(xpath = "//button[@id='home' and contains(text(),'Goto Home')]")
 	WebElement GoHomeButton;
-	
+
 	@FindBy(xpath = "//button[@id='position' and contains(text(),'Find Location')]")
 	WebElement FindLocationButton;
-	
+
 	@FindBy(xpath = "//button[@id='color' and contains(text(),'What is my color')]")
 	WebElement WhatisMyColorButton;
-	
-	
+
+	@FindBy(xpath = "//button[@id='property' and contains(text(),'How tall & fat I am?')]")
+	WebElement HowTallAndFatButton;
+
+
 	public ButtonPage() {
 		PageFactory.initElements(driver, this);
-		
+
 	}
-	
+
 	public void ClickonGotoHomePageButton() {
 		GoHomeButton.click();
 	}
-	
+
 	public String GetLocationOfTheButton() {
 		Point location =  FindLocationButton.getLocation();
 		return location.toString();
-		
+
 	}
 
-	
+	public String GettheButtonColor() {
+		String ButtonColor = WhatisMyColorButton.getCssValue("color");
+		return ButtonColor;
+
+	}
+
+	public String GetTheWidthAndHeightofTheButton() {
+		Dimension size = HowTallAndFatButton.getSize();
+		return size.toString();
+	}
+	public int GetTheHeightofTheButton() {
+		Dimension size = HowTallAndFatButton.getSize();
+		return size.getHeight();
+	}
+	public int GetTheWidthofTheButton() {
+		Dimension size = HowTallAndFatButton.getSize();
+		return size.getWidth();
+	}
+
+
 
 }
--- src/test/java/com/letcode/testcases/ButtonPageTest.java
+++ src/test/java/com/letcode/testcases/ButtonPageTest.java
@@ -33,7 +33,7 @@ public class ButtonPageTest extends Basesteps{
 	}
 	
 	@Test(priority = 1)
-	public void VerifyGotoHomepageButton() {
+	public void VerifyGotoHomepageButtonTest() {
 		workspacepage = homepage.ClickonWorkSpaceOption();
 		buttonpage = workspacepage.ClickonClickButtonToOpenButtonpage();
 		buttonpage.ClickonGotoHomePageButton();
@@ -42,10 +42,33 @@ public class ButtonPageTest extends Basesteps{
 	}
 	
 	@Test(priority = 2)
-	public void verifyFindLocationButton() {
+	public void VerifyFindLocationButtonTest() {
 		workspacepage = homepage.ClickonWorkSpaceOption();
 		buttonpage = workspacepage.ClickonClickButtonToOpenButtonpage();
 		assertEquals(buttonpage.GetLocationOfTheButton(), "(123, 336)","The location is not displayed");
+		
+	}
+	
+	@Test(priority = 3)
+	public void VerifyButtonColorTest() {
+		workspacepage = homepage.ClickonWorkSpaceOption();
+		buttonpage = workspacepage.ClickonClickButtonToOpenButtonpage();
+		assertEquals(buttonpage.GettheButtonColor(), ("rgba(0, 0, 0, 1)"), "The colors value is not displayed");
+	}
+	
+	@Test(priority = 4)
+	public void VerifyTheButtonWidthAndHeight() {
+		workspacepage = homepage.ClickonWorkSpaceOption();
+		buttonpage = workspacepage.ClickonClickButtonToOpenButtonpage();
+		assertEquals(buttonpage.GetTheWidthAndHeightofTheButton(),"(174, 40)", "The Width and Height of the button is not Fetched");
+		
+	}
+	
+	@Test(priority = 5)
+	public void VerifyOnlyTheHeightofTheButton() {
+		workspacepage = homepage.ClickonWorkSpaceOption();
+		buttonpage = workspacepage.ClickonClickButtonToOpenButtonpage();
+		assertEquals(buttonpage.GetTheHeightofTheButton(), 40,"The Height is not Fetched");;
 		
 	}
 	
